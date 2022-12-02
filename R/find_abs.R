0edee5ef8d7b04536abc828d6f03d9df27516f07
biogeojake <jacobtperez14@gmail.com>
2022-12-02 13:30:45 -0500

    biogeojake - Set find_abs() to take a unique wavelength

--- R/find_abs.R
+++ R/find_abs.R
@@ -2,15 +2,18 @@
 #'
 #'@description Retrieves the absorbance value for FerroZine measurements
 #'
+#'@param txtfile Takes in a character value that corresponds to a textfile with spectral data
+#'@param wl Takes in a numeric value that corresponds to the wavelength you would like to extract absorbance values from. The default value is 562 for FerroZine assays
 #'
+#'@export
 
-find_abs <- function(txtfile){
+find_abs <- function(txtfile, wl = 562){
 
   #Obtain data frame of spectral values
   spectrum <- get_spec(txtfile)
 
   #Retrieve wavelength closest to wl and correspdoning absorbance
-  df <- spectrum[which.min(abs(562 - spectrum$wavelength)),]
+  df <- spectrum[which.min(abs(wl - spectrum$wavelength)),]
 
   #Report a data frame of the wavelength and absorbance
   df
--- tests/testthat/test-find_abs.R
+++ tests/testthat/test-find_abs.R
@@ -1,3 +1,3 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("finds right absorbance", {
+  expect_equal("GB22_601_Absorbance__0__16-31-45-076.txt", )
 })
