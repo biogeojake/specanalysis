667e21da6b7dd9ef6da8066f0d48546bd29390e2
biogeojake <jacobtperez14@gmail.com>
2022-12-03 15:14:39 -0500

    biogeojake - analyze_set() now takes in directory information

--- R/analyze_set.R
+++ R/analyze_set.R
@@ -3,21 +3,28 @@
 #'@description Retrieves the absorbance values for a set of samples or standards
 #'
 #'@param df Takes in a dataframe object that contains multiple rows with files to analyze spectra from
+#'@param home Takes in a character object that corresponds to the directory with all the files
+#'@param specdir Takes in a character object that corresponds to the subdirectory that contains all the spectral files
 #'
 #'@export
 
-analyze_set <- function(df){
+analyze_set <- function(df, home = getwd(), specdir = "./raw_fz_files"){
 
-  #Create a data frame that will have the dataframe with wavelength and absorbance
-  #B <- lapply(X = df[ , col.num], FUN = find_abs)
+  # Move down to the spectral file directory
+  setwd(specdir)
 
-  B.test <- df$filename %>%
+  #Create a df of wavelength and absorbance from each sample
+  wave_ab <- df$filename %>%
     map(find_abs)
+
   #Combine the components into one clean dataframe
-  B <- do.call("rbind", B)
+  wave_ab <- do.call("rbind", wave_ab)
 
   #Combine wavelength and absorbance to the original dataframe
-  df <- cbind(df, B)
+  df <- cbind(df, wave_ab)
+
+  #Move back up to the main directory
+  setwd(home)
 
   #Return the dataframe
   df
