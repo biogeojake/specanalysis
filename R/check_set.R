fdf5595a90fac604ea7e17a25797d7adf7c16a18
biogeojake <jacobtperez14@gmail.com>
2022-12-03 15:15:18 -0500

    biogeojake - Added check_set() to verify data sets are properly formatted

---
+++ R/check_set.R
@@ -0,0 +1,20 @@
+#'Check that the set is valid
+#'
+#'@description Checks if the filename column is explicitly written and ensures the format is correct
+#'
+#'@param df Takes in a dataframe object that corresponds to the sample or standard files
+#'
+#'@export
+
+check_set <- function(df){
+  #Check if set has a column with 'filename'
+  if (!('filename' %in% colnames(df))) {
+    print('Could not detect filename column. Please write file to include a filename header.')
+  } else {
+    #Check if set has proper file extensions and add it if not there
+    df$filename[!str_detect(df$filename, '.txt')] <- paste(df$filename, '.txt', sep = '')
+  }
+  print('Set has been cleaned')
+
+  df
+}
