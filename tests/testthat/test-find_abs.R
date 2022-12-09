965f7acf70c4a5e44bdffed7d45c0af720bcf458
biogeojake <jacobtperez14@gmail.com>
2022-12-09 12:50:37 -0500

    biogeojake - Uploading the unit test for find_abs()

--- tests/testthat/test-find_abs.R
+++ tests/testthat/test-find_abs.R
@@ -1,3 +1,9 @@
 test_that("finds right absorbance", {
-  expect_equal("GB22_601_Absorbance__0__16-31-45-076.txt", )
+  abs <- find_abs("test_spectrum.txt")
+
+  #Test that it finds the most appropriate wavelength:
+  expect_equal(abs[1, 1], 562.09325)
+
+  #Test that it finds the corresponding absorbance:
+  expect_equal(abs[1, 2], 0.045783)
 })
