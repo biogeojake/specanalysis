5d375312662052faa96fcfd6fd92cee4e79fcb47
biogeojake <jacobtperez14@gmail.com>
2022-12-09 13:15:17 -0500

    biogeojake - Upload unit test for analyze_set()

--- tests/testthat/test-analyze_set.R
+++ tests/testthat/test-analyze_set.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("multiple analysese works", {
+  #Import tidyverse to use map
+  library(tidyverse)
+
+  #Create a test set
+  set <- data.frame(conc = c(0, 5),
+                    filename = c("test_spectrum.txt", "test_spectrum2.txt"))
+
+  #Analyze the set
+  p_set <- analyze_set(set, home = getwd(), specdir = getwd())
+
+  #Test that the correct wavelength and abosrbance were pulled:
+  expect_equal(p_set[1, 3], 562.09325)
+  expect_equal(p_set[1, 4], 0.045783)
+  expect_equal(p_set[2, 3], 562.09325)
+  expect_equal(p_set[2, 4], 0.2338661)
 })
