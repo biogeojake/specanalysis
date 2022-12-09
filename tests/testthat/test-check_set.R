7bbdf43b46f76d77b5d15ad332cc7f823cb69c19
biogeojake <jacobtperez14@gmail.com>
2022-12-09 14:28:52 -0500

    biogeojake - Upload unit test for check_set()

--- tests/testthat/test-check_set.R
+++ tests/testthat/test-check_set.R
@@ -1,3 +1,38 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("checks for correct output", {
+  #Import tidyverse to use map
+  library(tidyverse)
+
+  #Create a test set
+  set <- data.frame(conc = c(0, 5),
+                    filename = c("test_spectrum", "test_spectrum2.txt"))
+
+  expect_output(check_set(set), 'Set has been cleaned')
+
+})
+
+test_that("checks for correct bad output", {
+  #Import tidyverse to use map
+  library(tidyverse)
+
+  #Create a test set
+  bad_set <- data.frame(conc = c(0, 5),
+                    file = c("test_spectrum", "test_spectrum2.txt"))
+
+  expect_output(check_set(bad_set), 'Could not detect filename column. Please write file to include a filename header.')
+
+})
+
+test_that("checks for cleaned filenames", {
+  #Import tidyverse to use map
+  library(tidyverse)
+
+  #Create a test set
+  set <- data.frame(conc = c(0, 5),
+                    filename = c("test_spectrum", "test_spectrum2.txt"))
+
+  checked_set <- check_set(set)
+
+  expect_equal(checked_set[1, 2], "test_spectrum.txt")
+  expect_equal(checked_set[2, 2], "test_spectrum2.txt")
+
 })
