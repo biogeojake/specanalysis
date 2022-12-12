44eb6a007c66c38681014266f001cb4553fb9034
biogeojake <jacobtperez14@gmail.com>
2022-12-11 19:11:53 -0500

    biogeojake - Upload unit test for calibrate_set()

--- tests/testthat/test-calibrate_set.R
+++ tests/testthat/test-calibrate_set.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("gets good calibration curve values", {
+
+  #Create a test set that has an intercept of 5 and a slope of 50
+  test_stds <- data.frame(absorbance = c(0, 0.25, 0.5, 1),
+                          concentration = c(5, 17.5, 30, 55))
+
+  #Use calbiration function
+  calibration <- calibrate_set(test_stds)
+
+  #Check for appropriate coefficients
+  expect_equal(calibration[["absorbance"]], 50)
+  expect_equal(calibration[["(Intercept)"]], 5)
+
 })
